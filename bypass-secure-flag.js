d37a14bf4ff4259663a668f189d269eb6380c9cd
João <joaovictorti08@gmail.com>
2023-03-08 11:21:45 -0300

    Atualizações

--- bypass-secure-flag.js
+++ bypass-secure-flag.js
@@ -1,18 +1,33 @@
-Java.perform(function () {
-	var surface_view = Java.use("android.view.Surface_view");
-	var set_secure = surface_view.setSecure.overload("boolean");
-	set_secure.implementation = function (flag) {
-		console.log("setSecure() flag called with args: " + flag);
-		set_secure.call(false);
-	};
+Java.perform(function() {
+    var surface_view = Java.use('android.view.SurfaceView');
 
-	var window = Java.use("android.view.Window");
-	var set_flags = window.setFlags.overload("int","int");
-	var window_manager = Java.use("android.view.WindowManager");
-	var layout_params = Java.use("android.view.WindowManager$LayoutParams");
+    var set_secure = surface_view.setSecure.overload('boolean');
 
-	set_flags.implementation = function (flags, mask) {
-		flags = (flags.value & ~layout_params.FLAG_SECURE.value);
-		set_flags.call(this,flags,mask);
-	};
+    set_secure.implementation = function(flag){
+        console.log("setSecure() flag called with args: " + flag); 
+        set_secure.call(false);
+    };
+
+    var window = Java.use('android.view.Window');
+    var set_flags = window.setFlags.overload('int', 'int');
+
+    var window_manager = Java.use('android.view.WindowManager');
+    var layout_params = Java.use('android.view.WindowManager$LayoutParams');
+
+    set_flags.implementation = function(flags, mask){
+        //console.log(Object.getOwnPropertyNames(window.__proto__).join('\n'));
+        //console.log("flag secure: " + layout_params.FLAG_SECURE.value);
+
+        var oflag = flags;
+        //console.log("before setflags called  flags:  "+ flags);
+        flags =(flags.value & ~layout_params.FLAG_SECURE.value);
+        //console.log("after setflags called  flags:  "+ flags);
+
+        if (oflag != flags){
+            console.log('[+] Bypassing Flag Secure');
+        }
+
+        set_flags.call(this, flags, mask);
+        
+    };
 });
